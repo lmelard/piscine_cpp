733a6ae2458e017e3dd5db90cc832bae89c4cae4
Lena MELARD <lmelard@e3r9p14.clusters.42paris.fr>
2022-12-21 20:02:18 +0100

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,13 +6,37 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 17:26:10 by lmelard           #+#    #+#             */
-/*   Updated: 2022/12/20 17:26:42 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/12/21 19:15:27 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <ctype.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)(std::toupper(argv[i][j]));
+				j++;
+			}
+			if (i != argc - 1)
+				std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
