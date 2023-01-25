d971063055b8a778fd5f84ce93190cdf7223b92b
Lena MELARD <lmelard@e3r8p18.clusters.42paris.fr>
2023-01-25 17:28:43 +0100

    cool

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 18:03:26 by lmelard           #+#    #+#             */
-/*   Updated: 2023/01/19 18:04:57 by lmelard          ###   ########.fr       */
+/*   Updated: 2023/01/25 16:52:14 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,20 @@
 class	Fixed 
 {
 	public:
-		Fixed(void);
-		~Fixed(void);
-	private:	
+		Fixed(void); // constructeur par defaut
+		Fixed(Fixed const & src); // constructeur de recopie
+		~Fixed(void); // destructeur
+
+		Fixed & operator=(Fixed const & rhs);
+		int		getRawBits(void) const;
+		void	setRawBits( int const raw );
+	private:
+		int					_rawBits;
+		static int const	_bitsNbr;
 };
 
 #endif
+
+// nombre litteral = valeur numerique qui ne change pas au cours de l'execution d'un programme
+// peuvent etre des int ou des float
+// ne peuvent pas etre modifiees apres avoir ete definies
---
+++ cpp02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 15:23:16 by lmelard           #+#    #+#             */
+/*   Updated: 2023/01/25 15:25:28 by lmelard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
