554c91df5d7b511fff5185d6cf192c94826f7465
Lena MELARD <lmelard@e1r6p18.clusters.42paris.fr>
2023-01-26 14:11:42 +0100

    Makefile

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:33:27 by lenamelard        #+#    #+#             */
-/*   Updated: 2023/01/12 16:21:33 by lmelard          ###   ########.fr       */
+/*   Updated: 2023/01/26 13:21:34 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,15 @@ int main()
 		jim.attack(); 
 		club.setType("some other type of club");
 		jim.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB chris("Chris");
+		chris.attack(); 
+		chris.setWeapon(club);
+		chris.attack(); 
+		club.setType("some other type of club");
+		chris.attack();
 	}
 	return 0;
 }
