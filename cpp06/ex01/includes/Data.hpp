9b1671b03537606e634b211e09cbccc351fc65d7
Lena MELARD <lmelard@e1r10p16.clusters.42paris.fr>
2023-02-11 12:45:07 +0100

    cpp06/ex01

--- cpp06/ex01/includes/Data.hpp
+++ cpp06/ex01/includes/Data.hpp
@@ -6,20 +6,20 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 22:12:28 by lmelard           #+#    #+#             */
-/*   Updated: 2023/02/10 22:26:53 by lmelard          ###   ########.fr       */
+/*   Updated: 2023/02/11 12:22:21 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DATA_HPP
 # define DATA_HPP
 
-class Data
+# include <stdint.h>
+# include <iostream>
+
+typedef struct s_data
 {
-	public:
-		Data(void);
-		~Data();
-	private:
-		/* data */
-};
+	int	i;
+	
+} Data;
 
 #endif
--- cpp06/ex01/srcs/main.cpp
+++ cpp06/ex01/srcs/main.cpp
@@ -6,20 +6,44 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 22:10:20 by lmelard           #+#    #+#             */
-/*   Updated: 2023/02/10 22:40:36 by lmelard          ###   ########.fr       */
+/*   Updated: 2023/02/11 12:44:10 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Data.hpp"
+
+/* A pointer can be converted to any integral type large enough 
+** to hold all values of its type (e.g. to std::uintptr_t)
+*/
 
 uintptr_t	serialize(Data* ptr)
 {
-	
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-int	main(int ac, char **av)
+Data*	deserialize(uintptr_t raw)
 {
-	(void)ac
-	(void)av;
+	return (reinterpret_cast<Data*>(raw));
+}
+
+int	main(void)
+{
+	Data *data = new Data;
+	data->i = 42;
+	uintptr_t ptr;
+	Data *data2 = NULL;
+
+	std::cout << "data2 address: " << data2 << std::endl;
+	std::cout << "data address: " << data << std::endl;
+	std::cout << "data->i: " << data->i << std::endl;
+	ptr = serialize(data);
+	std::cout << "uintptr_t ptr: " << ptr << std::endl;
+	data2 = deserialize(ptr);
+	std::cout << "data2 address: " << data2 << std::endl;
+	std::cout << "data2->i: " << data2->i << std::endl;
+	data->i++;
+	std::cout << "data2->i: " << data2->i << std::endl;
+
+	delete data;
 	return (0);
 }
